24a0eca75983c620f4adda908f4c06071ef6ad4d
smistry4 <smistry1@vols.utk.edu>
2024-03-03 23:11:09 -0500

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import Home from "./routes/Home";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
